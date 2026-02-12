9cdc55c880def95bd5b7038e93e1fd47d5d70642
Yo <macarenapadii@gmail.com>
2026-02-12 12:31:39 +0100

    Version Inicial

--- examenGit.java
+++ examenGit.java
@@ -1,10 +1,7 @@
  
  public class examenGit { public static void main(String[] args) {  int numeros=1000;for(int i=1; i<=numeros; i++){ multiplos(i);} } 
  public static void multiplos(int numeros) {
-            if(i%7==0){
-                System.out.println("Múltiplo de 7: "+i);
-            }
-    
+           
             if(i%49==0){
                 System.out.println("Múltiplo de 49: "+i);
             }
