153d55cd27832c7e97ed287e8cef308f45ff2212
Yo <macarenapadii@gmail.com>
2026-02-12 12:25:01 +0100

    Multiplos

---
+++ examenGit.java
@@ -0,0 +1,9 @@
+ public static void multiplos(int numeros) {
+            if(i%49==0){
+                System.out.println("Múltiplo de 49: "+i);
+            }
+            else if(i%100==0){
+                System.out.println("Múltiplo de 100: "+i);
+            }
+        }
+    
