3d570f081a45adb5c847a8fc3f68b81bdf91317b
Yo <macarenapadii@gmail.com>
2026-02-12 12:35:20 +0100

    Error

--- examenGit.java
+++ examenGit.java
@@ -3,7 +3,7 @@
  public static void multiplos(int numeros) {
             if(i%7==0){
                 System.out.println("Múltiplo de 7: "+i);
-                
+
             }
             if(i%49==0){
                 System.out.println("Múltiplo de 49: "+i);
@@ -12,5 +12,5 @@
                 System.out.println("Múltiplo de 100: "+i);
             }
         }
-    
+     
     
