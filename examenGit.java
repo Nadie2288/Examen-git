c4fbc9ced96ea3ea0258341bd43f548e47f39cb3
Yo <macarenapadii@gmail.com>
2026-02-12 12:36:54 +0100

    Ahora error

--- examenGit.java
+++ examenGit.java
@@ -1,5 +1,5 @@
  
- public class examenGit { public static void main(String[] args) {  int numeros=1000;for(int i=1; i<=numeros; i++){ multiplos(i);} } 
+ public static void main(String[] args) {   
  public static void multiplos(int numeros) {
             if(i%7==0){
                 System.out.println("Múltiplo de 7: "+i);
@@ -12,5 +12,5 @@
                 System.out.println("Múltiplo de 100: "+i);
             }
         }
-     
+    }
     
